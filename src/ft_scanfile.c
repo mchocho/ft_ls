d2f66193ee39d18dc73f70b96239f6a39aa494f6
sal'e chocho <malachocho01@gmail.com>
2019-12-04 02:47:54 +0200

    Minor adjustments

--- src/ft_scanfile.c
+++ src/ft_scanfile.c
@@ -40,20 +40,6 @@ static int	ft_skipfile(char *path, flagobject *flagship)
 	return (!flagship->f_flag && !flagship->a_flag && fn[0] == '.');
 }
 
-/*static int	ft_iscorpdir(t_file *file)
-{
-	char *str;
-	if (ft_filetype(file->file_status) != 'd')
-		return (false);
-	ft_putendl("\nHello ft_iscorpdir()");
-	if (ft_strcmp(file->filename, "./") == 0)
-		return (true);
-	str = ft_splicepath(file->filename);
-	ft_putstr("ft_iscorpdir splicepath returned url: ");
-	ft_putendl(str);
-	return (ft_strcmp(str, ".") == 0 || ft_strcmp(str, "..") == 0);
-}*/
-
 static int	ft_iscorpdir(char *path)
 {
 	char *str;
@@ -83,7 +69,6 @@ void ft_scanfile(char *path, flagobject *flagship)
 		while ((entry = readdir(directory)) != NULL)
 		{
 			absp = ft_strjoin(ft_parseurl(path), ft_splicepath(entry->d_name));
-			//ft_putendl(absp);
 			if (lstat(absp, &fstat) < 0 || ft_skipfile(absp, flagship))
 				continue;
 			ft_addtail(list, absp, &fstat);
@@ -110,57 +95,18 @@ void ft_scanfile(char *path, flagobject *flagship)
 		list->current = list->head;
 		while (list->current != NULL)
 		{
-			absp = list->current->filename;//ft_strjoin(ft_parseurl(path), ft_splicepath(list->current->filename));
+			absp = list->current->filename;
 			if (ft_ispathdir(absp) && !ft_skipfile(absp, flagship) && !ft_iscorpdir(list->current->filename)) {
 				absp = ft_strjoin(ft_parseurl(path), ft_splicepath(list->current->filename));
 				ft_putstr("\n\n'");
-				ft_putstr(/*ft_strippath(*/absp/*)*/);
+				ft_putstr(absp);
 				ft_putstr("':\n");
-				/*if (flagship->l_flag)
-				{
-					ft_putstr("Total: ");
-					//ft_totalsize(absp, true);
-					ft_putchar('\n');
-				}*/
 				ft_scanfile(absp, flagship);
 				ft_strcleandel(&absp);
 			}
 			list->current = list->current->next;
 		}
 	}
-	//ft_putendl("Cleaning the linked list");
 	ft_cleanlist(&list);
-	//ft_putendl("And we're done cleaning the list!");
 	return ;
 }
-
-/*int main(int argc, char **argv)
-{
-	ft_putstr(" Testing ft_scanfile.c\n----------------------------------\n");
-
-	char *foo;
-	flagobject *flagship = (flagobject *)malloc(sizeof(flagobject));
-
-	ft_initflagobject(flagship);
-	
-	flagship->l_flag = false;
-	flagship->a_flag = true;
-	flagship->f_flag = false;
-	flagship->R_flag = true;
-	flagship->r_flag = false;
-	flagship->t_flag = false;
-	flagship->d_flag = false;
-	flagship->u_flag = false;
-	
-	if (argc > 1)
-		foo = argv[1];
-	else foo = "./";
-
-	ft_putstr("Scanning file: ");
-	ft_putstr(foo);
-	ft_putstr("\n\n");
-
-	ft_scanfile(foo, flagship);
-
-	return 0;
-}*/
